04f92347de7406e67a16b6c9a524af08ab43694f
Kayden2410 <dattabishaldatta@gmail.com>
2026-03-13 19:21:06 +0530

    First Commit

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "N Queens Game",
+  description: "Solve the N Queens puzzle",
+  manifest: "/manifest.json"
+};
+export const viewport = {
+  themeColor: "#7c3aed"
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
